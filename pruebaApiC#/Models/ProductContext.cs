d2ec794d1523c708d71c018dd761207ee14de371
Javier-Cs <krchipullajavier@gmail.com>
2025-06-19 15:07:21 -0500

    implementacion de la clase productContext - actúa como el puente principal entre tu aplicación y la base de datos. - Permite realizar operaciones CRUD

---
+++ pruebaApiC#/Models/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace pruebaApiC_.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options): base(options) { 
+        }
+
+        public DbSet<ProductModel> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ProductModel>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
--- pruebaApiC#/Models/ProductModel.cs
+++ pruebaApiC#/Models/ProductModel.cs
@@ -5,17 +5,17 @@ namespace pruebaApiC_.Models
 {
     public class ProductModel
     {
-        private int Id { get; set; }
+        public int Id { get; set; }
 
         [MaxLength(50, ErrorMessage = "El nombre no puede exceder los 50 caracteres.")]
-        private string Name { get; set; } = null;
+        public string Name { get; set; } = null;
 
         [DataType(DataType.MultilineText)]
         [MaxLength(200, ErrorMessage = "La descripción no puede exceder los 200 caracteres.")]
-        private string Description { get; set; } = null!;
+        public string Description { get; set; } = null!;
 
         [Column(TypeName ="decimal(18,2)")]
         [DisplayFormat(DataFormatString ="{0:C2}")]
-        private decimal Price { get; set; }
+        public decimal Price { get; set; }
     }
 }
