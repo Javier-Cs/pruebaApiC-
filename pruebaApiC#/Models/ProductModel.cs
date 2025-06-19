6663beb3b8ae18cf2f2c4696a5c4c6962690c526
Javier-Cs <krchipullajavier@gmail.com>
2025-06-19 14:55:18 -0500

    implementacion de 2 paquetes Nutget como - Microsoft.EntityFrameworkCore.SqlServer - Microsoft.EntityframeworkCore.Tools esto sirve para la conexion de la base de datos SQLServer - creacion de una entidad, uso de null en los atributos - uso de notaciones de validacion como MaxLength y DataType para los strings - uso de Column para el decimal

---
+++ pruebaApiC#/Models/ProductModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace pruebaApiC_.Models
+{
+    public class ProductModel
+    {
+        private int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "El nombre no puede exceder los 50 caracteres.")]
+        private string Name { get; set; } = null;
+
+        [DataType(DataType.MultilineText)]
+        [MaxLength(200, ErrorMessage = "La descripción no puede exceder los 200 caracteres.")]
+        private string Description { get; set; } = null!;
+
+        [Column(TypeName ="decimal(18,2)")]
+        [DisplayFormat(DataFormatString ="{0:C2}")]
+        private decimal Price { get; set; }
+    }
+}
